0883c43ee94e1cf8826424a20d5a3dcfa4a4056c
YuviPanda <yuvipanda@gmail.com>
2014-08-24 19:56:26 +0100

    Add a ipynb notebook viewer

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from IPython.config import Config
+from IPython.nbconvert import HTMLExporter
+from IPython.nbformat import current as nbformat
+
+app = Flask(__name__)
+
+@app.route('/<string:user>/<path:path>')
+def display(user, path):
+    full_path = os.path.join('/home', user, 'notebooks', path + '.ipynb')
+    exportHtml = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
+    notebook = nbformat.reads_json(open(full_path).read())
+    body, res = exportHtml.from_notebook_node(notebook)
+    return body
+
+if __name__ == '__main__':
+    app.run(debug=True)

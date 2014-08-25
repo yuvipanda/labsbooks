0c75a539783a52062a8c876dfef3784d1633f1e9
YuviPanda <yuvipanda@gmail.com>
2014-08-25 02:35:41 +0100

    Return 404 with message if no notebook is found

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 @app.route('/<string:user>/<path:path>')
 def display(user, path):
     full_path = os.path.join('/home', user, 'notebooks', path + '.ipynb')
+    if not os.path.exists(full_path):
+        return "No such notebook", 404
     exportHtml = HTMLExporter(config=Config({'HTMLExporter':{'default_template':'basic'}}))
     notebook = nbformat.reads_json(open(full_path).read())
     body, res = exportHtml.from_notebook_node(notebook)
